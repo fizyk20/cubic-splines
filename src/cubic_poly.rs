efd052945a8a869c25f0f647818a4e48090d62e4
Bartłomiej Kamiński <fizyk20@gmail.com>
2020-05-10 01:40:03 +0200

    Implement splines with the derivatives boundary condition

---
+++ src/cubic_poly.rs
@@ -0,0 +1,64 @@
+use std::ops;
+
+#[derive(Clone, Copy, Debug)]
+pub struct CubicPoly {
+    a: f64,
+    b: f64,
+    c: f64,
+    d: f64,
+}
+
+impl CubicPoly {
+    pub fn new(a: f64, b: f64, c: f64, d: f64) -> Self {
+        Self { a, b, c, d }
+    }
+
+    /// Creates a polynomial g(x) = f(x-x0), where f(x) = self
+    pub fn shifted(self, x0: f64) -> Self {
+        let a = self.a;
+        let b = self.b - 3.0 * self.a * x0;
+        let c = self.c + 3.0 * self.a * x0 * x0 - 2.0 * self.b * x0;
+        let d = self.d - self.a * x0 * x0 * x0 + self.b * x0 * x0 - self.c * x0;
+        Self { a, b, c, d }
+    }
+
+    pub fn eval(&self, x: f64) -> f64 {
+        self.a * x * x * x + self.b * x * x + self.c * x + self.d
+    }
+}
+
+impl ops::AddAssign<CubicPoly> for CubicPoly {
+    fn add_assign(&mut self, other: CubicPoly) {
+        self.a += other.a;
+        self.b += other.b;
+        self.c += other.c;
+        self.d += other.d;
+    }
+}
+
+impl ops::SubAssign<CubicPoly> for CubicPoly {
+    fn sub_assign(&mut self, other: CubicPoly) {
+        self.a -= other.a;
+        self.b -= other.b;
+        self.c -= other.c;
+        self.d -= other.d;
+    }
+}
+
+impl ops::Add<CubicPoly> for CubicPoly {
+    type Output = CubicPoly;
+
+    fn add(mut self, other: CubicPoly) -> CubicPoly {
+        self += other;
+        self
+    }
+}
+
+impl ops::Sub<CubicPoly> for CubicPoly {
+    type Output = CubicPoly;
+
+    fn sub(mut self, other: CubicPoly) -> CubicPoly {
+        self -= other;
+        self
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,7 @@
-pub struct CubicPoly {
-    a: f64,
-    b: f64,
-    c: f64,
-    d: f64,
-}
+mod cubic_poly;
 
-impl CubicPoly {
-    pub fn new(a: f64, b: f64, c: f64, d: f64) -> Self {
-        Self { a, b, c, d }
-    }
-
-    /// Creates a polynomial g(x) = f(x-x0), where f(x) = self
-    pub fn shifted(self, x0: f64) -> Self {
-        let a = self.a;
-        let b = self.b - 3.0 * self.a * x0;
-        let c = self.c + 3.0 * self.a * x0 * x0 - 2.0 * self.b * x0;
-        let d = self.d - self.a * x0 * x0 * x0 + self.b * x0 * x0 - self.c * x0;
-        Self { a, b, c, d }
-    }
-
-    pub fn eval(&self, x: f64) -> f64 {
-        self.a * x * x * x + self.b * x * x + self.c * x + self.d
-    }
-}
+pub use cubic_poly::CubicPoly;
+use std::iter;
 
 pub enum BoundaryCondition {
     Derivatives(f64, f64),
@@ -31,6 +10,7 @@ pub enum BoundaryCondition {
     Periodic,
 }
 
+#[derive(Clone, Debug)]
 pub struct Spline {
     points_x: Vec<f64>,
     splines: Vec<CubicPoly>,
@@ -39,13 +19,63 @@ pub struct Spline {
 }
 
 impl Spline {
-    pub fn new(points: Vec<(f64, f64)>, boundary_condition: BoundaryCondition) -> Self {
-        // TODO
+    fn with_derivatives(points: Vec<(f64, f64)>, d0: f64, dn: f64) -> Self {
+        let n = points.len();
+        let points_x: Vec<f64> = points.iter().map(|&(x, _)| x).collect();
+        let points_y: Vec<f64> = points.iter().map(|&(_, y)| y).collect();
+        let h: Vec<f64> = points_x.windows(2).map(|w| w[1] - w[0]).collect();
+
+        let a: Vec<f64> = iter::once(0.0)
+            .chain(h.windows(2).map(|w| w[0] / (w[0] + w[1])))
+            .chain(iter::once(1.0))
+            .collect();
+        let b: Vec<f64> = iter::repeat(2.0).take(points_x.len()).collect();
+        let c: Vec<f64> = iter::once(1.0)
+            .chain(h.windows(2).map(|w| w[1] / (w[0] + w[1])))
+            .chain(iter::once(0.0))
+            .collect();
+        let d: Vec<f64> = iter::once(6.0 / h[0] * (div_diff_2(points[0], points[1]) - d0))
+            .chain(
+                points
+                    .windows(3)
+                    .map(|w| 6.0 * div_diff_3(w[0], w[1], w[2])),
+            )
+            .chain(iter::once(
+                6.0 / h[n - 2] * (dn - div_diff_2(points[n - 2], points[n - 1])),
+            ))
+            .collect();
+        let second_derivatives = solve_tridiagonal(a, b, c, d);
+        let splines = (0..n - 1)
+            .map(|i| {
+                let hi = h[i];
+                let mi = second_derivatives[i];
+                let mi1 = second_derivatives[i + 1];
+                let yi = points_y[i];
+                let yi1 = points_y[i + 1];
+                let poly1 =
+                    CubicPoly::new(-mi / 6.0 / hi, 0.0, -(yi - mi * hi * hi / 6.0) / hi, 0.0)
+                        .shifted(points_x[i + 1]);
+                let poly2 =
+                    CubicPoly::new(mi1 / 6.0 / hi, 0.0, (yi1 - mi1 * hi * hi / 6.0) / hi, 0.0)
+                        .shifted(points_x[i]);
+                poly1 + poly2
+            })
+            .collect();
         Self {
-            points_x: Default::default(),
-            splines: Default::default(),
-            derivative_start: 0.0,
-            derivative_end: 0.0,
+            points_x,
+            splines,
+            derivative_start: d0,
+            derivative_end: dn,
+        }
+    }
+
+    pub fn new(mut points: Vec<(f64, f64)>, boundary_condition: BoundaryCondition) -> Self {
+        points.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+        match boundary_condition {
+            BoundaryCondition::Derivatives(d0, dn) => Self::with_derivatives(points, d0, dn),
+            BoundaryCondition::SecondDerivatives(_d0, _dn) => unimplemented!(),
+            BoundaryCondition::Natural => unimplemented!(),
+            BoundaryCondition::Periodic => unimplemented!(),
         }
     }
 
@@ -76,6 +106,30 @@ impl Spline {
             }
         }
     }
+}
+
+fn div_diff_2((x0, y0): (f64, f64), (x1, y1): (f64, f64)) -> f64 {
+    (y1 - y0) / (x1 - x0)
+}
+
+fn div_diff_3((x0, y0): (f64, f64), (x1, y1): (f64, f64), (x2, y2): (f64, f64)) -> f64 {
+    y0 / (x0 - x1) / (x0 - x2) + y1 / (x1 - x0) / (x1 - x2) + y2 / (x2 - x0) / (x2 - x1)
+}
+
+fn solve_tridiagonal(a: Vec<f64>, mut b: Vec<f64>, c: Vec<f64>, mut d: Vec<f64>) -> Vec<f64> {
+    let n = b.len();
+    for i in 1..n {
+        let w = a[i] / b[i - 1];
+        b[i] -= w * c[i - 1];
+        d[i] -= w * d[i - 1];
+    }
+    let mut result = vec![d[n - 1] / b[n - 1]];
+    for i in (0..n - 1).rev() {
+        let last_x = result[result.len() - 1];
+        result.push((d[i] - c[i] * last_x) / b[i]);
+    }
+    result.reverse();
+    result
 }
 
 #[cfg(test)]
